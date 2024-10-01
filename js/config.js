fd2ac0c875b9650866ff31b028a53c2827826e5d
Manju Ashish Buddhaghosh <43272580+thebuddhaghosh@users.noreply.github.com>
2024-10-01 18:25:41 +0530

    menu changes

--- js/config.js
+++ js/config.js
@@ -1,5 +1,20 @@
-const toggler = document.querySelector(".sidemenu-toggle");
-toggler.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("collapsed");
-  $("#sidemenuDemo").toggleClass("collapsed");
+// const toggler = document.querySelector(".sidemenu-toggle");
+// toggler.addEventListener("click", function () {
+//   document.querySelector("#sidebar").classList.toggle("collapsed");
+//   $("#sidemenuDemo").toggleClass("collapsed");
+// });
+
+$(document).ready(function () {
+  $(".sidemenu-toggle").click(function () {
+    var mainnav = $("#sidebar");
+    if (mainnav.hasClass("mainnav-lg")) {
+      $("#sidemenuDemo").removeClass("mainnav-lg").addClass("mainnav-sm");
+      mainnav.removeClass("mainnav-lg").addClass("mainnav-sm");
+      // handleMenuHover();
+    } else {
+      $("#sidemenuDemo").removeClass("mainnav-sm").addClass("mainnav-lg");
+      mainnav.removeClass("mainnav-sm").addClass("mainnav-lg");
+      // handleMenuHover();
+    }
+  });
 });
--- js/nav-popover.js
+++ js/nav-popover.js
@@ -1,40 +1,102 @@
-$(document).ready(function () {
-  console.log("sidenav js included");
-  function handleMenuHover() {
-    if (!$("#sidebar").hasClass("collapsed")) {
-      $(".sidebar-nav li a").hover(
-        function () {
-          console.log("hover");
-          var titleText = $(this).attr("nav-title");
-          if (titleText) {
-            var tooltip = $('<div class="tooltip-menu"></div>').text(titleText).appendTo("body");
+// $(document).ready(function () {
+//   function applyHoverEff() {
+//     $(".mainnav-lg #mainnav-menu > li").off("mouseenter mouseleave");
 
-            // Position the tooltip near the mouse cursor
-            $(this).mousemove(function (event) {
-              tooltip.css({
-                top: event.pageY + 10 + "px",
-                position: "absolute",
-                background: "#333", // Optional: style the tooltip
-                color: "#fff",
-                padding: "15px",
-                zIndex: 99999,
-              });
-            });
-          }
-        },
-        function () {
-          // Remove the tooltip when the mouse leaves
-          $(".tooltip-menu").remove();
-        }
-      );
-    } else if (!$("#sidebar").hasClass("collapsed")) {
-      // Unbind hover event if the "mainnav-lg" class exists
-      $(".sidebar-nav li a").unbind("mouseenter mouseleave");
+//     $(".mainnav-sm #mainnav-menu > li").hover(
+//       function () {
+//         // On mouse enter, add the hover effect class
+//         $(this).find("ul.collapse").removeClass("collapse");
+//         menuText = $(this).find("a.sidebar-link").attr("title");
+//         $(this).find(".sidebar-item").addClass("hover");
+//         $(this)
+//           .find(".sidebar-item")
+//           .prepend('<h3 class="menu-title-bg">' + menuText + "</h3>");
+//       },
+//       function () {
+//         // On mouse leave, remove the hover effect class
+
+//         $(this).find("div").addClass("collapse");
+//         $(this).find(".sidebar-item .menu-title-bg").remove();
+//         $(this).find(".sidebar-item").removeClass("hover");
+//       }
+//     );
+//   }
+
+//   applyHoverEff();
+//   $(".sidemenu-toggle").click(
+//     (expanMenu = () => {
+//       applyHoverEff();
+//       if ($(".mainnav-sm").length == 1) {
+//         $(".sidebar-link").removeClass("collapsed");
+//         $(".sidebar-item").removeClass("show");
+//       }
+//     })
+//   );
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  function handleMouseEnter(item) {
+    const submenu = item.querySelector("ul.sidebar-dropdown");
+    const menuText = item.querySelector("a.sidebar-link").getAttribute("nav-title");
+
+    // Show the submenu and add hover effects
+    if (submenu) {
+      submenu.classList.remove("collapse");
+    }
+    const sidebarItem = item.querySelector(".sidebar-item");
+    if (sidebarItem) {
+      sidebarItem.classList.add("hover");
+      const titleElement = document.createElement("h3");
+      titleElement.className = "menu-title-bg";
+      titleElement.textContent = menuText;
+      sidebarItem.prepend(titleElement);
     }
   }
 
-  handleMenuHover();
-  $(window).resize(function () {
-    handleMenuHover();
-  });
+  function handleMouseLeave(item) {
+    const submenu = item.querySelector("ul.sidebar-dropdown");
+    const sidebarItem = item.querySelector(".sidebar-item");
+
+    // Hide the submenu and remove hover effects
+    if (submenu) {
+      submenu.classList.add("collapse");
+    }
+    if (sidebarItem) {
+      const titleElement = sidebarItem.querySelector(".menu-title-bg");
+      if (titleElement) {
+        titleElement.remove();
+      }
+      sidebarItem.classList.remove("hover");
+    }
+  }
+
+  function applyHoverEff() {
+    const menuItems = document.querySelectorAll(".sidebar-nav .sidebar-item");
+
+    menuItems.forEach((item) => {
+      item.addEventListener("mouseenter", function () {
+        handleMouseEnter(item);
+      });
+
+      item.addEventListener("mouseleave", function () {
+        handleMouseLeave(item);
+      });
+    });
+  }
+
+  applyHoverEff();
+
+  const sidemenuToggle = document.querySelector(".sidemenu-toggle");
+  if (sidemenuToggle) {
+    sidemenuToggle.addEventListener("click", function () {
+      applyHoverEff();
+      const mainnavSm = document.querySelector("#sidebar");
+      if (mainnavSm) {
+        const sidebarLinks = document.querySelectorAll(".sidebar-link");
+        sidebarLinks.forEach((link) => link.classList.remove("collapsed"));
+        const sidebarItems = document.querySelectorAll(".sidebar-item");
+        sidebarItems.forEach((item) => item.classList.remove("show"));
+      }
+    });
+  }
 });
